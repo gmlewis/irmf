e06f43c384259cd4fb8ca00a36630b8fc4551988
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-03-17 09:09:06 -0400

    Clean up examples

--- update-examples.sh
+++ update-examples.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
 go run cmd/update-examples/main.go
 pt -l '##' examples/*/README.md | sort | sed -e 's|/README.md||'
+
+# Validate IRMF shader syntax:
+irmf-slicer examples/*/*.irmf
