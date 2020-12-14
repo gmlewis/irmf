e00bb64ba3393e8f43a18dc6a76f8b8c4a893245
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-13 21:32:30 -0500

    Fix spirals

--- examples/012-bifilar-electromagnet/aprbfem.go
+++ examples/012-bifilar-electromagnet/aprbfem.go
@@ -1,4 +1,4 @@
-// -*- compile-command: "go run aprbfem.go -n 12"; -*-
+// -*- compile-command: "go run aprbfem.go -n 36"; -*-
 
 // aprbfem creates an axial-plus-radial-bi-filar-electro-magnet
 // STL file using Go. It is based on 30x30x132mm-vert.irmf.
@@ -92,21 +92,22 @@ func (m *arBifilarElectromagnet) coilPlusConnectorWires(wireNum, coilNum int) {
 func (m *arBifilarElectromagnet) coilSquareFace(wireNum int, radius, trimStartAngle, trimEndAngle float64) {
 	delta := 2.0 * math.Pi / float64(*numDiv)
 	angle := 0.0
-	if wireNum%2 == 0 {
-		angle = math.Pi
-	}
 	endAngle := float64(m.numTurns)*2.0*math.Pi + angle
 
 	ri := radius - 0.5*m.size
 	ro := radius + 0.5*m.size
 	for ; angle <= endAngle-delta; angle += delta {
-		m.coilWireSegment(angle, angle+delta, ri, ro)
+		m.coilWireSegment(wireNum, angle, angle+delta, ri, ro)
 	}
 }
 
-func (m *arBifilarElectromagnet) coilWireSegment(a1, a2, ri, ro float64) {
-	z1 := a1 / (2.0 * math.Pi)
-	z2 := a2 / (2.0 * math.Pi)
+func (m *arBifilarElectromagnet) coilWireSegment(wireNum int, a1, a2, ri, ro float64) {
+	z1 := a1 / math.Pi
+	z2 := a2 / math.Pi
+	if wireNum%2 == 0 {
+		a1 += math.Pi
+		a2 += math.Pi
+	}
 
 	pu := func(r, a, z float64) *vec3.T {
 		return &vec3.T{float32(r * math.Cos(a)), float32(r * math.Sin(a)), float32(z - 0.5*m.size)}
@@ -125,14 +126,14 @@ func (m *arBifilarElectromagnet) coilWireSegment(a1, a2, ri, ro float64) {
 	p2do := pd(ro, a2, z2)
 	// p2di := pd(ri, a2, z2)
 
-	// n1o := vec3.T{float32(math.Cos(a1)), float32(math.Sin(a1)), 0}
+	n1o := vec3.T{float32(math.Cos(a1)), float32(math.Sin(a1)), 0}
 	n1u := vec3.Cross(pu(ro, a1, z1-1.0).Sub(p1uo), cp(p2uo).Sub(p1uo))
 	n1u.Normalize()
 	// n1i := vec3.T{float32(math.Cos(a1 + math.Pi)), float32(math.Sin(a1 + math.Pi)), 0}
 	n1d := vec3.Cross(pd(ro, a1, z1+1.0).Sub(p1do), cp(p2do).Sub(p1do))
 	n1d.Normalize()
 
-	n2o := vec3.T{float32(math.Cos(a2)), float32(math.Sin(a2)), 0}
+	// n2o := vec3.T{float32(math.Cos(a2)), float32(math.Sin(a2)), 0}
 	n2u := vec3.Cross(pu(ro, a2, z2-1.0).Sub(p2uo), cp(p2uo).Sub(p2uo))
 	n2u.Normalize()
 	// n2i := vec3.T{float32(math.Cos(a2 + math.Pi)), float32(math.Sin(a2 + math.Pi)), 0}
@@ -147,6 +148,6 @@ func (m *arBifilarElectromagnet) coilWireSegment(a1, a2, ri, ro float64) {
 			V3: [3]float32{v3[0], v3[1], v3[2]},
 		})
 	}
-	write(&n2o, p1uo, p2do, p1do)
-	write(&n2o, p1uo, p2uo, p2do)
+	write(&n1o, p1uo, p2do, p1do)
+	write(&n1o, p1uo, p2uo, p2do)
 }
